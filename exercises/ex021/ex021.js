e6cf442f31aaf11fdc299c657855c2773b25e00f
Henrique Umeda <issamuumeda@gmail.com>
2021-01-11 21:23:28 -0300

    Update ex021.js

--- exercises/ex021/ex021.js
+++ exercises/ex021/ex021.js
@@ -2,10 +2,15 @@ function tabuada() {
     let numero = document.getElementById('numero')
     let tabuada = document.querySelector('div#resultado')
     let valorNumero = Number(numero.value)
-    tabuada.innerHTML = ''
 
-    for (let contador = 1; contador <= 10; contador++) {
-        let valorResultado = Number(contador) * valorNumero
-        tabuada.innerHTML += `${valorNumero}x${contador}=${valorResultado}<br/>`
+    if (numero.value.length == 0) {
+        window.alert('Por favor, digite um número!') 
+        tabuada.innerHTML = 'Digite um número acima'
+    } else {
+        tabuada.innerHTML = ''
+        for (let contador = 1; contador <= 10; contador++) {
+            let valorResultado = Number(contador) * valorNumero
+            tabuada.innerHTML += `${valorNumero}x${contador}=${valorResultado}<br/>`
+        }
     }
 }
