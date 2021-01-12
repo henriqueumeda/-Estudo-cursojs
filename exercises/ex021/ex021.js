2627247eee902fab7367c1a9972e2ba1bd187f90
Henrique Umeda <issamuumeda@gmail.com>
2021-01-11 21:21:00 -0300

    Update ex021.js

--- exercises/ex021/ex021.js
+++ exercises/ex021/ex021.js
@@ -1,10 +1,11 @@
 function tabuada() {
     let numero = document.getElementById('numero')
     let tabuada = document.querySelector('div#resultado')
-    let valorNumero = Number(numero)
+    let valorNumero = Number(numero.value)
+    tabuada.innerHTML = ''
 
     for (let contador = 1; contador <= 10; contador++) {
-        let valorResultado = contador * valorNumero
-        tabuada.innerHTML = `${valorNumero}x${contador}=${valorResultado}<br/>`
+        let valorResultado = Number(contador) * valorNumero
+        tabuada.innerHTML += `${valorNumero}x${contador}=${valorResultado}<br/>`
     }
 }
