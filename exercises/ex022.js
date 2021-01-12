0c1d1009e3c95545b4e0fff6f052b5f17f0361ef
Henrique Umeda <issamuumeda@gmail.com>
2021-01-12 20:51:17 -0300

    Update ex022.js

--- exercises/ex022.js
+++ exercises/ex022.js
@@ -1,3 +1,16 @@
 let num = [5, 8, 2, 9, 3]
 
-console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+num.push(4)
+console.log(`O número ${num[5]} foi adicionado na ${num.length}ª posição`)
+console.log(`A order crescente do array é ${num.sort()}`)
+
+let valor = 1
+let posicao = num.indexOf(valor)
+
+if (posicao == -1) {
+    console.log(`O valor ${valor} não foi encontrado!`)
+} else {
+    console.log(`O valor ${valor} está na posição ${posicao}`)
+}
